9937d78b70e1767fac9ab57ab86f79b7bde9bdbb
lily <lily@nihility.cn>
2020-12-14 19:14:46 +0800

    add log4j2 and change edit config

--- spring-nihility/src/main/java/cn/nihility/boot/start/SpringStarter.java
+++ spring-nihility/src/main/java/cn/nihility/boot/start/SpringStarter.java
@@ -1,11 +1,15 @@
 package cn.nihility.boot.start;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringStarter {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringStarter.class);
 
 	public static void main(String[] args) {
 		final AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
@@ -17,6 +21,8 @@ public class SpringStarter {
 
 		final Object bean1 = ctx.getBean("innerConfigClass3");
 		System.out.println(bean1);
+
+		log.info("Bean1 [{}]", bean1);
 
 		ctx.registerShutdownHook();
 	}
